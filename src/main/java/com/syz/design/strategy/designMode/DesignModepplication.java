f2e49c8b30354936a9a3a8e378061c08e88849ef
shaoyingzhu <1037185543@qq.com>
2020-06-19 17:52:02 +0800

    初始化工程

---
+++ src/main/java/com/syz/design/strategy/designMode/DesignModepplication.java
@@ -0,0 +1,13 @@
+package com.syz.design.strategy.designMode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModepplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModepplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syz/design/strategy/designMode/DesignModepplicationTests.java
@@ -0,0 +1,13 @@
+package com.syz.design.strategy.designMode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModepplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
